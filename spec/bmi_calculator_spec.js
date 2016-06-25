34fc37510d5c94c439b3e15f9312dfeb23b6f500
Lucia De Mojà <luciademoja@gmail.com>
2016-06-25 14:28:16 +0200

    Added tests for the implemented switch for imperial and metric cases

--- spec/bmi_calculator_spec.js
+++ spec/bmi_calculator_spec.js
@@ -2,15 +2,33 @@ describe("BMICalculator", function() {
   var bmi_calculator;
   var person;
 
+describe("Metric", function() {
   beforeEach(function() {
     person = new Person({weight: 90, height: 186});
     calculator = new BMICalculator();
+
   });
 
   it("calculates BMI for a person using metric method", function() {
-  calculator.metric_bmi(person);
+    person.calc_type = 'metric';
+    person.calculate_bmi();
   expect(person.bmiValue).toEqual(26.01);
   });
 
-  
+});
+
+describe("Imperial", function() {
+  beforeEach(function() {
+    person = new Person({weight: 130 , height: 68});
+    calculator = new BMICalculator();
+  });
+
+  it("calculates BMI for a person using imperial method", function() {
+    person.calc_type = 'imperial';
+    person.calculate_bmi();
+  expect(person.bmiValue).toEqual(19.76);
+  });
+
+});
+
 });
--- spec/bmi_ui_spec.js
+++ spec/bmi_ui_spec.js
@@ -8,11 +8,23 @@ describe('BMI_UI - index.html', function() {
     $('#calculate').trigger('click');
   });
 
-  it("displays BMI Value", function() {
+  it("displays Metric BMI Value", function() {
+    person.calc_type = 'metric';
     expect($('#display_value').text()).toBe('Your BMI is 26.01');
   });
 
-  it("display BMI Message", function() {
+  it("display Metric BMI Message", function() {
+    person.calc_type = 'metric';
     expect($('#display_message').text()).toBe('and you are Overweight');
+  });
+
+  it("displays Imperial BMI Value", function() {
+    person.calc_type = 'imperial';
+    expect($('#display_value').text()).toBe('Your BMI is 19.76');
+  });
+
+  it("display Imperial BMI Message", function() {
+    person.calc_type = 'imperial';
+    expect($('#display_message').text()).toBe('and you are Normal');
   });
 });
